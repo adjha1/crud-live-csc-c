38c09822ad200d84ca3b8a054d1ad7b7fcb56351
Aditya Kumar Jha <aditya.ibm.kumar@gmail.com>
2025-05-01 14:54:48 +0530

    crud-live-cse-c

--- frontend/adi/src/component/Delete.jsx
+++ frontend/adi/src/component/Delete.jsx
@@ -5,7 +5,7 @@ function Delete() {
     const adi=async(e)=>{
         e.preventDefault
         const id=e.target.id.value;
-        await axios.delete(`http://localhost:9000/users/${id}`)
+        await axios.delete(`https://crud-live-csc-c-backend.onrender.com/users/${id}`)
         alert("data delete")
     }
   return (
